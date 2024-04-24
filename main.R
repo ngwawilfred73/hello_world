f3e553458d79e3d760ba4681b95d91f033cbbc1e
ngwawilfred73 <118343591+ngwawilfred73@users.noreply.github.com>
2024-04-24 05:58:59 +0100

    capitalise each word

--- main.R
+++ main.R
@@ -1,1 +1,1 @@
-print("hello Kenny. You are a great teacher :), although I love all the other teachers too. I also love the students as well")
+print("Hello Kenny. You Are A Great Teacher :), Although I Love All The Other Teachers Too. I Also Love The Students As Well")
